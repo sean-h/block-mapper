83480a63c60dea5a10a97a55129cd47d64258430
Sean Humeniuk <seanhumeniuk@gmail.com>
2017-12-20 22:24:28 -0500

    Add stb_image.h

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include "Shader.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
