53c111ad8b506d7cf30159ccb967fbf5fbbbbea0
Sean Humeniuk <seanhumeniuk@gmail.com>
2017-12-25 14:59:37 -0500

    Add assimp library

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -10,6 +10,10 @@
 #include "glm\gtc\type_ptr.hpp"
 #include "Camera.h"
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void processInput(GLFWwindow *window);
