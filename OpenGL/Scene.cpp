839ecfd43a0a4482651bba027f63881741224377
Sean Humeniuk <seanhumeniuk@gmail.com>
2018-02-17 15:37:29 -0500

    Remove default scene entities

--- OpenGL/Scene.cpp
+++ OpenGL/Scene.cpp
@@ -5,31 +5,6 @@
 
 Scene::Scene()
 {
-	std::vector<glm::vec3> cubePositions;
-	cubePositions.push_back(glm::vec3(0.0f, 0.0f, 0.0f));
-	cubePositions.push_back(glm::vec3(2.0f, 5.0f, -15.0f));
-	cubePositions.push_back(glm::vec3(-1.0f, -2.0f, -2.0f));
-	cubePositions.push_back(glm::vec3(-4.0f, -2.0f, -12.0f));
-	cubePositions.push_back(glm::vec3(2.0f, 0.0f, -3.0f));
-	cubePositions.push_back(glm::vec3(2.0f, 0.0f, -4.0f));
-	cubePositions.push_back(glm::vec3(3.0f, 0.0f, -3.0f));
-	cubePositions.push_back(glm::vec3(2.0f, -0.0f, -3.0f));
-	cubePositions.push_back(glm::vec3(-2.0f, 3.0f, -7.0f));
-	cubePositions.push_back(glm::vec3(1.0f, -2.0f, -2.0f));
-	cubePositions.push_back(glm::vec3(1.0f, 2.0f, -2.0f));
-	cubePositions.push_back(glm::vec3(1.0f, 0.0f, -1.0f));
-	cubePositions.push_back(glm::vec3(-1.0f, 1.0f, -1.0f));
-
-	for (auto p : cubePositions)
-	{
-		auto entityHandle = this->CreateEntity();
-		Entity* entity = entityHandle->TargetEntity();
-		entity->ObjectTransform()->Position(p);
-		entity->MeshName("Cube");
-		entity->ColliderMeshName("Cube");
-		entity->MaterialName("Solid");
-	}
-
 	auto cameraEntityHandle = this->CreateEntity();
 	Entity* cameraEntity = cameraEntityHandle->TargetEntity();
 	cameraEntity->ObjectTransform()->Position(glm::vec3(0.0f, 0.0f, -10.0f));
