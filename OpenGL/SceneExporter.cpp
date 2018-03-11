ffb2b93666c93009dfd241561a148f90a2348e9c
Sean Humeniuk <seanhumeniuk@gmail.com>
2018-03-10 22:52:20 -0500

    Put entities into ExportObjects before exporting scene

--- OpenGL/SceneExporter.cpp
+++ OpenGL/SceneExporter.cpp
@@ -1,4 +1,5 @@
 #include "SceneExporter.h"
+#include "glm\gtc\matrix_transform.hpp"
 #include <vector>
 #include <set>
 
@@ -44,76 +45,42 @@ void SceneExporter::Export(ApplicationContext * context)
 	{
 		Mesh* mesh = allModels[usedMesh.first]->GetMesh(usedMesh.second);
 		exportScene.mMeshes[meshIndex] = new aiMesh();
-		this->LoadMeshData(&exportScene, mesh, meshIndex);
+		ExportMeshData meshData = this->GetMeshData(mesh);
+		std::string meshName = usedMesh.first + std::to_string(usedMesh.second);
+		exportMeshes[meshName] = std::make_shared<ExportMeshData>(meshData);
+		exportMeshesList.push_back(exportMeshes[meshName]);
+		this->AddMeshDataToScene(&exportScene, &meshData, meshIndex);
 		meshIndexes[usedMesh.first][usedMesh.second] = meshIndex;
 		meshIndex++;
 	}
 
-	// Add all entities as children to the root node
-	int exportEntityCount = 0;
 	for (auto &e : context->ApplicationScene()->Entities())
 	{
 		if (e->HasProperty("Block"))
 		{
-			exportEntityCount += 2;
+			CreateExportObject(e.get());
 		}
 	}
 
-	aiNode** children = new aiNode*[exportEntityCount];
-	int entityIndex = 0;
-	for (auto &e : context->ApplicationScene()->Entities())
+	aiNode** children = new aiNode*[exportObjects.size()];
+	for (int i = 0; i < exportObjects.size(); i++)
 	{
-		if (!e->HasProperty("Block"))
-		{
-			continue;
-		}
-
-		children[entityIndex] = new aiNode();
-		children[entityIndex]->mMeshes = new unsigned int[1];
-		children[entityIndex]->mMeshes[0] = meshIndexes[e->MeshName()][e->MeshColorIndex()];
-		children[entityIndex]->mNumMeshes = 1;
-
-		children[entityIndex]->mName = e->MeshName() + "_" + std::to_string(e->ID());
-
-		aiMatrix4x4 translationMatrix;
-		glm::vec3 position = e->ObjectTransform()->Position();
-		aiVector3D entityPosition(position.x, position.y, position.z);
-		aiMatrix4x4::Translation(entityPosition, translationMatrix);
-
-		aiMatrix4x4 rotationMatrix;
-		glm::quat rotation = e->ObjectTransform()->RotationQuaternion();
-		float rotationAngle = glm::angle(rotation);
-		glm::vec3 rotationAxis = glm::axis(rotation);
-		aiVector3D aiRotationAxis(rotationAxis.x, rotationAxis.y, rotationAxis.z);
-		aiMatrix4x4::Rotation(rotationAngle, aiRotationAxis, rotationMatrix);
-
-		aiMatrix4x4 transformation = translationMatrix * rotationMatrix;
-
-		children[entityIndex]->mTransformation = transformation;
-		entityIndex++;
-
-		// Collision mesh
-		children[entityIndex] = new aiNode();
-		children[entityIndex]->mMeshes = new unsigned int[1];
-		children[entityIndex]->mMeshes[0] = meshIndexes[e->ColliderMeshName()][0];
-		children[entityIndex]->mNumMeshes = 1;
-
-		children[entityIndex]->mName = "Collider_" + e->MeshName() + "_" + std::to_string(e->ID());
-		children[entityIndex]->mTransformation = transformation;
-
-		entityIndex++;
+		children[i] = new aiNode();
+		children[i]->mMeshes = new unsigned int[1];
+		children[i]->mMeshes[0] = this->GetExportMeshDataID(exportObjects[i].exportMeshData);
+		children[i]->mNumMeshes = 1;
+		children[i]->mName = exportObjects[i].name;
+		children[i]->mTransformation = exportObjects[i].transformMatrix;
 	}
-
-	exportScene.mRootNode->addChildren(exportEntityCount, children);
+	exportScene.mRootNode->addChildren(exportObjects.size(), children);
 
 	Assimp::Exporter exporter;
 	exporter.Export(&exportScene, "collada", exportFilePath);
 }
 
-void SceneExporter::LoadMeshData(aiScene* scene, Mesh * mesh, int meshIndex)
+ExportMeshData SceneExporter::GetMeshData(Mesh * mesh)
 {
-	scene->mMeshes[meshIndex]->mNumVertices = mesh->vertices.size();
-	scene->mMeshes[meshIndex]->mVertices = new aiVector3D[mesh->vertices.size()];
+	ExportMeshData meshData;
 
 	for (int i = 0; i < mesh->vertices.size(); i++)
 	{
@@ -121,31 +88,27 @@ void SceneExporter::LoadMeshData(aiScene* scene, Mesh * mesh, int meshIndex)
 		vertPos.x = mesh->vertices[i].Position.x;
 		vertPos.y = mesh->vertices[i].Position.y;
 		vertPos.z = mesh->vertices[i].Position.z;
-		scene->mMeshes[meshIndex]->mVertices[i] = vertPos;
+		meshData.vertexPositions.push_back(vertPos);
 	}
 
-	scene->mMeshes[meshIndex]->mNormals = new aiVector3D[mesh->vertices.size()];
 	for (int i = 0; i < mesh->vertices.size(); i++)
 	{
 		aiVector3D vertNormal;
 		vertNormal.x = mesh->vertices[i].Normal.x;
 		vertNormal.y = mesh->vertices[i].Normal.y;
 		vertNormal.z = mesh->vertices[i].Normal.z;
-		scene->mMeshes[meshIndex]->mNormals[i] = vertNormal;
+		meshData.vertexNormals.push_back(vertNormal);
 	}
 
-	scene->mMeshes[meshIndex]->mTextureCoords[0] = new aiVector3D[mesh->vertices.size()];
 	for (int i = 0; i < mesh->vertices.size(); i++)
 	{
 		aiVector3D vertTexCoords;
 		vertTexCoords.x = mesh->vertices[i].TexCoords.x;
 		vertTexCoords.y = 1.0f - mesh->vertices[i].TexCoords.y;
 		vertTexCoords.z = 0.0f;
-		scene->mMeshes[meshIndex]->mTextureCoords[0][i] = vertTexCoords;
+		meshData.vertexTextureCoords.push_back(vertTexCoords);
 	}
 
-	scene->mMeshes[meshIndex]->mNumFaces = (unsigned int)(mesh->indices.size() / 3);
-	scene->mMeshes[meshIndex]->mFaces = new aiFace[mesh->indices.size() / 3];
 	int faceIndex = 0;
 	for (int i = 0; i < mesh->indices.size(); i += 3)
 	{
@@ -155,7 +118,111 @@ void SceneExporter::LoadMeshData(aiScene* scene, Mesh * mesh, int meshIndex)
 		face.mIndices[0] = mesh->indices[i];
 		face.mIndices[1] = mesh->indices[i + 1];
 		face.mIndices[2] = mesh->indices[i + 2];
-		scene->mMeshes[meshIndex]->mFaces[faceIndex] = face;
+		meshData.faces.push_back(face);
 		faceIndex++;
 	}
+
+	return meshData;
+}
+
+void SceneExporter::AddMeshDataToScene(aiScene * scene, ExportMeshData * meshData, int meshIndex)
+{
+	// Vertex Positions
+	scene->mMeshes[meshIndex]->mNumVertices = meshData->vertexPositions.size();
+	scene->mMeshes[meshIndex]->mVertices = new aiVector3D[meshData->vertexPositions.size()];
+	for (int i = 0; i < meshData->vertexPositions.size(); i++)
+	{
+		scene->mMeshes[meshIndex]->mVertices[i] = meshData->vertexPositions[i];
+	}
+
+	// Vertex Normals
+	scene->mMeshes[meshIndex]->mNormals = new aiVector3D[meshData->vertexNormals.size()];
+	for (int i = 0; i < meshData->vertexNormals.size(); i++)
+	{
+		scene->mMeshes[meshIndex]->mNormals[i] = meshData->vertexNormals[i];
+	}
+
+	// Texture Coordinates
+	scene->mMeshes[meshIndex]->mTextureCoords[0] = new aiVector3D[meshData->vertexTextureCoords.size()];
+	for (int i = 0; i < meshData->vertexTextureCoords.size(); i++)
+	{
+		scene->mMeshes[meshIndex]->mTextureCoords[0][i] = meshData->vertexTextureCoords[i];
+	}
+
+	// Faces
+	scene->mMeshes[meshIndex]->mNumFaces = meshData->faces.size();
+	scene->mMeshes[meshIndex]->mFaces = new aiFace[meshData->faces.size()];
+	for (int i = 0; i < meshData->faces.size(); i++)
+	{
+		scene->mMeshes[meshIndex]->mFaces[i] = meshData->faces[i];
+	}
+}
+
+void SceneExporter::CreateExportObject(Entity * entity)
+{
+	ExportObject exportObject;
+
+	std::string meshName = entity->MeshName() + std::to_string(entity->MeshColorIndex());
+	exportObject.exportMeshData = this->exportMeshes[meshName];
+	exportObject.name = entity->MeshName() + "_" + std::to_string(entity->ID());
+
+	aiMatrix4x4 transformation = TransformMatrix(
+		entity->ObjectTransform()->Position(),
+		entity->ObjectTransform()->RotationQuaternion(),
+		entity->ObjectTransform()->Scale());
+	exportObject.transformMatrix = transformation;
+	this->exportObjects.push_back(exportObject);
+
+	if (entity->ColliderMeshName() != "")
+	{
+		ExportObject colliderObject;
+		std::string colliderMeshName = entity->ColliderMeshName() + "0";
+		colliderObject.exportMeshData = this->exportMeshes[colliderMeshName];
+		colliderObject.name = "Collider_" + entity->MeshName() + "_" + std::to_string(entity->ID());
+		colliderObject.transformMatrix = transformation;
+		this->exportObjects.push_back(colliderObject);
+	}
+}
+
+int SceneExporter::GetExportMeshDataID(std::shared_ptr<ExportMeshData> exportMeshData)
+{
+	for (int i = 0; i < exportMeshesList.size(); i++)
+	{
+		if (exportMeshData == exportMeshesList[i])
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+void ExportMeshData::TransformVertexPositions(glm::mat4 transformMatrix)
+{
+	for (auto& vertexPosition : vertexPositions)
+	{
+		glm::vec4 v(vertexPosition.x, vertexPosition.y, vertexPosition.z, 1.0f);
+		glm::vec4 newPosition = transformMatrix * v;
+
+		vertexPosition.x = newPosition.x;
+		vertexPosition.y = newPosition.y;
+		vertexPosition.z = newPosition.z;
+	}
+}
+
+aiMatrix4x4 TransformMatrix(glm::vec3 position, glm::quat rotation, glm::vec3 scale)
+{
+	aiMatrix4x4 translationMatrix;
+	aiVector3D entityPosition(position.x, position.y, position.z);
+	aiMatrix4x4::Translation(entityPosition, translationMatrix);
+
+	aiMatrix4x4 rotationMatrix;
+	float rotationAngle = glm::angle(rotation);
+	glm::vec3 rotationAxis = glm::axis(rotation);
+	aiVector3D aiRotationAxis(rotationAxis.x, rotationAxis.y, rotationAxis.z);
+	aiMatrix4x4::Rotation(rotationAngle, aiRotationAxis, rotationMatrix);
+
+	aiMatrix4x4 transformation = translationMatrix * rotationMatrix;
+
+	return transformation;
 }
--- OpenGL/SceneExporter.h
+++ OpenGL/SceneExporter.h
@@ -5,6 +5,23 @@
 #include <memory>
 #include "Mesh.h"
 
+struct ExportMeshData
+{
+	std::vector<aiVector3D> vertexPositions;
+	std::vector<aiVector3D> vertexNormals;
+	std::vector<aiVector3D> vertexTextureCoords;
+	std::vector<aiFace> faces;
+
+	void TransformVertexPositions(glm::mat4 transformMatrix);
+};
+
+struct ExportObject
+{
+	std::string name;
+	std::shared_ptr<ExportMeshData> exportMeshData;
+	aiMatrix4x4 transformMatrix;
+};
+
 class SceneExporter
 {
 public:
@@ -13,6 +30,14 @@ public:
 private:
 	std::unique_ptr<aiScene> exportScene;
 	std::unordered_map<std::string, std::unordered_map<int, int>> meshIndexes;
+	std::unordered_map<std::string, std::shared_ptr<ExportMeshData>> exportMeshes;
+	std::vector<std::shared_ptr<ExportMeshData>> exportMeshesList;
+	std::vector<ExportObject> exportObjects;
 
-	void LoadMeshData(aiScene* scene, Mesh* mesh, int meshIndex);
+	ExportMeshData GetMeshData(Mesh* mesh);
+	void AddMeshDataToScene(aiScene* scene, ExportMeshData* meshData, int meshIndex);
+	void CreateExportObject(Entity* entity);
+	int GetExportMeshDataID(std::shared_ptr<ExportMeshData> exportMeshData);
 };
+
+aiMatrix4x4 TransformMatrix(glm::vec3 position, glm::quat rotation, glm::vec3 scale);
